d705253f943c1c6eb8faf6f800485f224fa5316d
emepyc <emepyc@gmail.com>
2013-10-09 15:29:03 +0100

    finished
    Former-commit-id: 602e06a4dd5a5da2bda5c43de837406853736d85

--- modules/t/emf.pl
+++ modules/t/emf.pl
@@ -4,14 +4,16 @@ use Data::Dumper;
 
 use Bio::EnsEMBL::IO::Parser::EMFParser;
 
-my $test_file = 'modules/t/Compara.13_eutherian_mammals_EPO.chr1_26.emf';
-
+#my $test_file = 'modules/t/Compara.13_eutherian_mammals_EPO.chr1_26.emf';
+my $test_file = 'modules/t/Homo_sapiens.GRCh37.73.resequencing.chromosome.21.emf';
 my $parser = Bio::EnsEMBL::IO::Parser::EMFParser->open($test_file);
 
 my $next_record = $parser->next;
 print STDERR "FORMAT: ", $parser->format(), "\n";
-
-#print STDERR "NEXT RECORD: $next_record\n";
-
+print STDERR "TREE: ", $parser->tree, "\n";
+print STDERR Dumper $parser->score_types;
 print STDERR Dumper $parser->sequences;
 
+while (my $column = $parser->get_next_column) {
+  print STDERR "Next column is: ", Dumper $column;
+}
