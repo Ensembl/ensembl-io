5ba65b744352e5bb035f536fe22e163ac362f727
Alessandro Vullo <avullo@ebi.ac.uk>
2014-07-10 16:42:57 +0100

    AUTOLOAD method to avoid writing getter methods for all the supported format specifiers, reported among the comments.

--- modules/Bio/EnsEMBL/IO/Parser/BLASTFormatter.pm
+++ modules/Bio/EnsEMBL/IO/Parser/BLASTFormatter.pm
@@ -21,74 +21,14 @@ package Bio::EnsEMBL::IO::Parser::BLASTFormatter;
 use strict;
 use warnings;
 use Data::Dumper;
-
-use base qw/Bio::EnsEMBL::IO::ColumnBasedParser/;
-
 use Bio::EnsEMBL::Utils::Exception qw/throw/;
 
+use base qw/Bio::EnsEMBL::IO::ColumnBasedParser/;
+our ($AUTOLOAD);
 
-sub open {
-  my ($caller, $filename, $format, @other_args) = @_;
-  my $class = ref($caller) || $caller;
- 
-  defined $filename or 
-    throw "Must provide name of the file to parse";
-  -e $filename and -f $filename or
-    throw "Check file $filename exists and is readable";
-
-  defined $format or 
-    throw "Must provide format used to produce blast formatted output";
-
-  $format =~ /^(\d+)/ or throw "Invalid output format, must begin with number";
-  my $alignment_view_option = $1;
-  $alignment_view_option == 6 || $alignment_view_option == 7 || $alignment_view_option == 10 or 
-    throw "Invalid alignment view option: must be either 6, 7 or 10";
-
-  $format =~ s/^\d+?//;
-  $format =~ s/^\s+?//;
-  # no format specifier detected, set to default: 
-  # "qseqid sseqid pident length mismatch gapopen qstart qend sstart send evalue bitscore"
-  # see http://home.cc.umanitoba.ca/~psgendb/birchhomedir/doc/NCBI/blast_formatter.txt
-  $format = "qseqid sseqid pident length mismatch gapopen qstart qend sstart send evalue bitscore"
-    unless $format;
-
-  my $delimiter = ($alignment_view_option == 6 or $alignment_view_option == 7)?'\t':',';
-
-  my $self = $class->SUPER::open($filename, 
-				 $delimiter, 
-				 alignment_view => $alignment_view_option, 
-				 format_specifier => $format);
-  
-  # metadata defaults
-  #  if ($self->{'params'}->{'mustReadMetadata'}) { }
-
-  # pre-load peek buffer
-  $self->next_block();
-    
-  return $self;
-}
-
-=head2 set_fields
-
-    Description: Setter for list of fields used in this format - uses the
-                  "public" (i.e. non-raw) names of getter methods
-    Returntype : Void
-
-=cut
-
-sub set_fields {
-  my $self = shift;
-  my $format = $self->{params}{format_specifier};
-  defined $format or throw "Undefined BLAST output format";
-  
-  $self->{'fields'} = [ split /\s+/, $format ];
-
-  for (my $i=0; $i<scalar @{$self->{fields}}; $i++) {
-    $self->{fields_index}{$self->{fields}[$i]} = $i;
-  }
-}
-
-
+#
+# From: http://home.cc.umanitoba.ca/~psgendb/birchhomedir/doc/NCBI/blast_formatter.txt
+#
 # Supported format specifiers:
 # qseqid : Query Seq-id
 # qgi : Query GI
@@ -124,9 +64,86 @@ sub set_fields {
 # qframe : Query frame
 # sframe : Subject frame
 # btop : Blast traceback operations (BTOP)
+#
 
-sub get_raw_qseqid {
+#
+# AUTOLOAD
+#
+sub AUTOLOAD {
+  my $self = shift;
+  my $method = $AUTOLOAD;
+  $method =~ s/.*:://;
+
+  return if $method !~ /^get_/;
+
+  $method =~ /get_raw_(.+?)$|get_(.+?)$/;
+  my $attr = $1;
+
+  throw("Invalid attribute method: ->$method()") 
+    unless exists $self->{fields_index}{$attr};
   
+  return $self->{record}[$self->{fields_index}{$attr}];
+}
+
+sub open {
+  my ($caller, $filename, $format, @other_args) = @_;
+  my $class = ref($caller) || $caller;
+ 
+  defined $filename or 
+    throw "Must provide name of the file to parse";
+  -e $filename and -f $filename or
+    throw "Check file $filename exists and is readable";
+
+  defined $format or 
+    throw "Must provide format used to produce blast formatted output";
+
+  $format =~ /^(\d+)/ or throw "Invalid output format, must begin with number";
+  my $alignment_view_option = $1;
+  $alignment_view_option == 6 || $alignment_view_option == 7 || $alignment_view_option == 10 or 
+    throw "Invalid alignment view option: must be either 6, 7 or 10";
+
+  $format =~ s/^\d+?//;
+  $format =~ s/^\s+?//;
+  # no format specifier detected, set to default: 
+  # "qseqid sseqid pident length mismatch gapopen qstart qend sstart send evalue bitscore"
+  # see http://home.cc.umanitoba.ca/~psgendb/birchhomedir/doc/NCBI/blast_formatter.txt
+  $format = "qseqid sseqid pident length mismatch gapopen qstart qend sstart send evalue bitscore"
+    unless $format;
+
+  my $delimiter = ($alignment_view_option == 6 or $alignment_view_option == 7)?'\t':',';
+
+  my $self = $class->SUPER::open($filename, 
+				 $delimiter, 
+				 alignment_view => $alignment_view_option, 
+				 format_specifier => $format);
+ 
+  # metadata defaults
+  #  if ($self->{'params'}->{'mustReadMetadata'}) { }
+
+  # pre-load peek buffer
+  $self->next_block();
+    
+  return $self;
+}
+
+=head2 set_fields
+
+    Description: Setter for list of fields used in this format - uses the
+                  "public" (i.e. non-raw) names of getter methods
+    Returntype : Void
+
+=cut
+
+sub set_fields {
+  my $self = shift;
+  my $format = $self->{params}{format_specifier};
+  defined $format or throw "Undefined BLAST output format";
+  
+  $self->{fields} = [ split /\s+/, $format ];
+
+  for (my $i=0; $i<scalar @{$self->{fields}}; $i++) {
+    $self->{fields_index}{$self->{fields}[$i]} = $i;
+  }
 }
 
 1;
