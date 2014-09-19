f9b5753c7042e232c673b39e5cfbbba8e440bdb5
William McLaren <wm2@ebi.ac.uk>
2014-09-19 13:07:19 +0100

    add -f to tabix cmd line as some versions of tabix spout warnings about the .tbi being older than the .vcf.gz even when it isn't

--- modules/Bio/EnsEMBL/IO/Parser/VCF4Tabix.pm
+++ modules/Bio/EnsEMBL/IO/Parser/VCF4Tabix.pm
@@ -51,7 +51,7 @@ sub open {
   my $delimiter = "\t";   
   my $self = $class->SUPER::open($filename,$other_args);
   
-  my $tabix_data = `tabix -H $filename`;
+  my $tabix_data = `tabix -f -h $filename 0:0-0`;
   foreach my $line (split("\n",$tabix_data)) {
     $self->Bio::EnsEMBL::IO::Parser::BaseVCF4::read_metadata($line);
   }
