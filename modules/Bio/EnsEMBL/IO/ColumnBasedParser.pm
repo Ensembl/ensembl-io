5745b9f9065d2fb0bc4461a991f15cb79c9c6e9d
Anne Lyle <annelyle@ebi.ac.uk>
2015-06-01 15:00:26 +0100

    Added setters and getters for maximum and minimum column count and a validation method that uses these values

--- modules/Bio/EnsEMBL/IO/ColumnBasedParser.pm
+++ modules/Bio/EnsEMBL/IO/ColumnBasedParser.pm
@@ -60,13 +60,15 @@ sub open {
 
     $self = $class->SUPER::open($filepath, @other_args);
     $self->set_fields;
+    $self->set_minimum_column_count;
+    $self->set_maximum_column_count;
     $self->{'delimiter'} = $delimiter;
     my @delimiters       = split('\|', $delimiter);
     $self->{'default_delimiter'} = $delimiters[0];
     return $self;
 }
 
-=head2 fields
+=head2 get_fields
 
     Description: Getter 
     Returntype : Arrayref
@@ -89,6 +91,63 @@ sub set_fields {
     throw("Method not implemented. This is really important");
 }
 
+=head2 get_minimum_column_count
+
+    Description: Getter 
+    Returntype : Integer
+
+=cut
+
+sub get_minimum_column_count {
+    my $self = shift;
+    return $self->{'min_col_count'};
+}
+
+=head2 set_minimum_column_count
+
+    Description: Placeholder for user-defined minimum field count (defaults to 1)
+    Returntype : Void 
+
+=cut
+
+sub set_minimum_column_count {
+    my $self = shift;
+    $self->{'min_col_count'} = 1;
+}
+
+=head2 get_maximum_column_count
+
+    Description: Getter 
+    Returntype : Integer
+
+=cut
+
+sub get_maximum_column_count {
+    my $self = shift;
+    return $self->{'max_col_count'};
+}
+
+=head2 set_maximum_column_count
+
+    Description: Sets maximum number of fields - defaults to the number of defined fields 
+                  but overridden in formats where columns are split to make extra fields
+    Returntype : Void 
+
+=cut
+
+sub set_maximum_column_count {
+    my $self = shift;
+    $self->{'max_col_count'} = scalar(@{$self->{'fields'}});
+}
+
+
+=head2 read_record
+
+    Description: Splits the current block along predefined delimiters
+    Returntype : Void 
+
+=cut
+
 
 =head2 read_record
 
@@ -102,6 +161,58 @@ sub read_record {
     my $self = shift;
     chomp $self->{'current_block'};
     $self->{'record'} = [ split($self->{'delimiter'},$self->{'current_block'}) ] ;
+}
+
+=head2 validate
+    
+    Description: Performs very basic validation on the content
+    Returntype: Boolean
+
+=cut
+
+sub validate {
+    my $self = shift;
+
+    my $valid   = 0;
+
+    while ($self->next) {
+
+      next if $self->is_metadata;
+
+      $self->read_record;
+
+      ## Check we have the minimum number of columns for this format
+      my $col_count = scalar(@{$self->{'record'}});
+
+      if ($col_count >= $self->get_minimum_column_count
+            && $col_count <= $self->get_maximum_column_count) {
+        $valid = 1;
+      }
+
+      if ($self->start =~ /\d+/ && $self->start > 0 && $self->end =~ /\d+/) {
+        $valid = 1;
+      }
+
+      ## Additional format-specific validation
+      if ($self->_validate) {
+        $valid = 1;
+      }
+      last;
+    }
+
+    return $valid;
+}
+
+=head2 _validate 
+
+    Description: Stub - most formats will require additional validation
+    Returntype: Boolean
+
+=cut
+
+sub _validate {
+    my $self = shift;
+    return 1;
 }
 
 #---------- OUTPUT METHODS --------------
