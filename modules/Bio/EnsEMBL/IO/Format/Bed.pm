1da37c594aeb60d228f78c6010685dbb6864fc7d
Anne Lyle <annelyle@ebi.ac.uk>
2016-07-29 14:49:15 +0100

    Preliminary definition of standard BED format

---
+++ modules/Bio/EnsEMBL/IO/Format/Bed.pm
@@ -0,0 +1,126 @@
+=pod
+
+=head1 LICENSE
+
+Copyright [1999-2015] Wellcome Trust Sanger Institute and the EMBL-European Bioinformatics Institute
+Copyright [2016] EMBL-European Bioinformatics Institute
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+=head1 NAME
+
+Bio::EnsEMBL::IO::Format::Bed - an object that defines and validates BED-based formats
+
+Note that BED is a very complex format with several sub-varieties and flexible field options!
+This module defines the standard 3+9 BED format - other derived formats require their own
+classes.
+
+=cut
+
+package Bio::EnsEMBL::IO::Format::Bed;
+
+use strict;
+use warnings;
+
+our %params = (
+                'delimiter'     => ['\t', '\s'],
+                'has_metadata'  => -1,
+                'metadata_info' => {
+                                    'name' => {
+                                                'optional'    => 0, 
+                                                'validate_as' => 'string',
+                                                'unique'      => 1,
+                                              },
+                                    'description' => {
+                                                'optional'    => 1, 
+                                                'validate_as' => 'string',
+                                              },
+                                    'priority' => {
+                                                'optional'    => 1, 
+                                                'validate_as' => 'integer',
+                                              },
+                                    'colour' => {
+                                                'optional'    => 1, 
+                                                'validate_as' => 'colour',
+                                              },
+                                    'itemRgb' => {
+                                                'optional'    => 1, 
+                                                'validate_as' => 'case_insensitive',
+                                                'values'      => ['on'],
+                                              },
+                                    'useScore' => {
+                                                'optional'    => 1, 
+                                                'validate_as' => 'boolean',
+                                              },
+                                  
+                                    },
+                'field_info'    => {
+                                    'chrom' => {
+                                                'accessor'    => 'seqname',
+                                                'validate_as' => 'string',
+                                                'optional'    => 0,  
+                                                },
+                                    'chromStart' => {
+                                                'accessor'    => 'start',
+                                                'validate_as' => 'integer',
+                                                'optional'    => 0,  
+                                                },
+                                    'chromEnd' => {
+                                                'accessor'    => 'end',
+                                                'validate_as' => 'integer',
+                                                'optional'    => 0,  
+                                                },
+                                    'name' => {
+                                                'accessor'    => 'name',
+                                                'validate_as' => 'string',
+                                                'optional'    => 1,  
+                                                },
+                                    'score' => {
+                                                'validate_as' => 'integer',
+                                                'range'       => [0, 1000],
+                                                'optional'    => 1,  
+                                                },
+                                    'strand' => {
+                                                'validate_as' => 'strand_plusminus',
+                                                'optional'    => 1,  
+                                                },
+                                    'thickStart' => {
+                                                'validate_as' => 'integer',
+                                                'optional'    => 1,  
+                                                },
+                                    'thickEnd' => {
+                                                'validate_as' => 'integer',
+                                                'optional'    => 1,  
+                                                },
+                                    'itemRgb' => {
+                                                'validate_as' => 'rgb_string',
+                                                'optional'    => 1,  
+                                                },
+                                    'blockCount' => {
+                                                'validate_as' => 'integer',
+                                                'optional'    => 1,  
+                                                },
+                                    'blockSizes' => {
+                                                'validate_as' => 'comma_separated',
+                                                'optional'    => 1,  
+                                                },
+                                    'blockStarts' => {
+                                                'validate_as' => 'comma_separated',
+                                                'optional'    => 1,  
+                                                },
+                                    },
+                'field_order'   => [qw(chrom chromStart chromEnd name score strand thickStart thickEnd itemRgb blockCount blockSizes blockStarts)],
+              );
+
+
+1;
