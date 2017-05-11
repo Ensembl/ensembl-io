64f94a25f2c18f057525f7d1545915f39be20a33
Alessandro Vullo <avullo@ebi.ac.uk>
2017-05-11 16:46:24 +0100

    [ENSCORESW-2296]. Use v335 of kent src to avoid Bio::DB::BigFile build problem.

--- travisci/get_dependencies.sh
+++ travisci/get_dependencies.sh
@@ -8,11 +8,11 @@ fi
 
 echo 'Getting HTSlib'
 if [ ! -d htslib ]; then
-  git clone --branch master --depth 1 https://github.com/samtools/htslib.git
+  git clone --branch 1.3.2 --depth 1 https://github.com/samtools/htslib.git
 fi
 
 echo 'Getting jksrc'
-if [ ! -f jksrc.zip ]; then
-  wget http://hgdownload.cse.ucsc.edu/admin/jksrc.zip
-  unzip -q jksrc.zip
+if [ ! -f v335_base.tar.gz ]; then
+  wget https://github.com/ucscGenomeBrowser/kent/archive/v335_base.tar.gz
+  tar xzf v335_base.tar.gz
 fi
