d54769fb83c8e04638cc1e7811f36f9718d867ee
Alessandro Vullo <avullo@ebi.ac.uk>
2017-05-11 17:15:43 +0100

    [ENSCORESW-2296]. Use v335 of kent src to avoid Bio::DB::BigFile build problem.

--- travisci/build_c.sh
+++ travisci/build_c.sh
@@ -15,7 +15,7 @@ export MYSQLINC=`mysql_config --include | sed -e 's/^-I//g'`
 export MYSQLLIBS=`mysql_config --libs`
 
 # Build kent src
-cd kent/src/lib
+cd kent-335_base/src/lib
 echo 'CFLAGS="-fPIC"' > ../inc/localEnvironment.mk
 make
 cd ../jkOwnLib
