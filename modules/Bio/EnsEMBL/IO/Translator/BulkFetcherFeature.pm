e9fbe36dd62626b4d52c9e0f4f81739d6a96cf05
Alessandro Vullo <avullo@ebi.ac.uk>
2017-05-24 14:48:51 +0100

    [ENSCORESW-1928]. Added methods to support writing a BulkFetcherFeature.

--- modules/Bio/EnsEMBL/IO/Translator/BulkFetcherFeature.pm
+++ modules/Bio/EnsEMBL/IO/Translator/BulkFetcherFeature.pm
@@ -42,17 +42,32 @@ use Bio::EnsEMBL::Utils::SequenceOntologyMapper;
 
 my %field_callbacks = (version         => 'version',
 		       production_name => 'production_name',
-		       uri             => 'uri',
+		       id_org_short    => 'id_org_short',
 		       type            => 'type',
-		       biotype         => 'biotype',
-		       so_term         => 'so_term',
+		       id              => 'id',
 		       name            => 'name',
 		       description     => 'description',
-		       id              => 'id',
+		       seq_region_name => 'seq_region_name',
+		       cs_name         => 'coord_system_name',
+		       cs_version      => 'coord_system_version',
+		       start           => 'start',
+		       end             => 'end',
+		       strand          => 'strand',
+		       biotype         => 'biotype',
+		       rank            => 'rank',
+		       taxon_id        => 'taxon_id',
+		       uri             => 'uri',
 		       synonyms        => 'synonyms',
 		       provenance      => 'provenance',
 		       homologues      => 'homologues',
-		       xrefs           => 'xrefs'
+		       xrefs           => 'xrefs',
+		       dbname          => 'dbname',
+		       homologues      => 'homologues',
+		       transcripts     => 'transcripts',
+		       exons           => 'exons',
+		       translations    => 'translations',
+		       protein_features => 'protein_features',
+		       so_term         => 'so_term'
 		      );
 
 =head2 new
@@ -64,7 +79,7 @@ my %field_callbacks = (version         => 'version',
 sub new {
   my ($class, %args) = @_;
   
-  my @required_args = qw/version production_name xref_mapping_file ontology_adaptor/;
+  my @required_args = qw/version production_name xref_mapping_file ontology_adaptor meta_adaptor/;
   my @missing_args;
   map { push @missing_args, $args{$_} unless exists $args{$_} } @required_args;
   confess "Missing arguments required by Bio::EnsEMBL::IO::Translator::Feature: " . join(',', @missing_args)
@@ -79,6 +94,9 @@ sub new {
 
   croak "Bio::EnsEMBL::IO::Translator::Feature requires an ontology adaptor"
     unless $args{ontology_adaptor}->isa('Bio::EnsEMBL::DBSQL::OntologyTermAdaptor');
+
+  croak "Bio::EnsEMBL::IO::Translator::Slice requires a meta adaptor"
+    unless $args{meta_adaptor}->isa('Bio::EnsEMBL::DBSQL::MetaContainer');
   
   $args{ontology_cache} = {};
   $args{mapping} = $xref_mapping;
@@ -114,10 +132,10 @@ sub ontology_adaptor {
   return $self->{ontology_adaptor};
 }
 
-# sub meta_adaptor {
-#   my $self = shift;
-#   return $self->{meta_adaptor};
-# }
+sub meta_adaptor {
+  my $self = shift;
+  return $self->{meta_adaptor};
+}
 
 sub ensembl_mapper {
   my $self = shift;
@@ -127,6 +145,13 @@ sub ensembl_mapper {
 sub biotype_mapper {
   my $self = shift;
   return $self->{biotype_mapper};
+}
+
+sub id_org_short {
+  my ($self, $db) = @_;
+  defined $db or croak "Undefined db";
+  
+  return $self->ensembl_mapper->identifier_org_short($db);
 }
 
 =head2 type
@@ -145,30 +170,19 @@ sub type {
   return $object->{type};
 }
 
-=head2 uri
+=head2 id
 
-    Description: 
+    Description: Wrapper around RDF feature id field
     Returntype : String
 
 =cut
 
-sub uri {
+sub id {
   my ($self, $object) = @_;
-
-  return feature_uri($self->id($object), $self->type($object));
-}
-
-=head2 biotype
-
-    Description: Wrapper around RDF feature biotype field
-    Returntype : String
-
-=cut
-
-sub biotype {
-  my ($self, $object) = @_;
+  exists $object->{id} and defined $object->{id}
+    or croak "Feature does not have id";
   
-  return $object->{biotype};
+  return $object->{id};
 }
 
 =head2 name
@@ -197,19 +211,126 @@ sub description {
   return $object->{description};
 }
 
-=head2 id
 
-    Description: Wrapper around RDF feature id field
+=head2 seq_region_name
+
+=cut
+
+sub seq_region_name {
+  my ($self, $object) = @_;
+  
+  return $object->{seq_region_name};
+}
+
+=head2 coord_system_name
+
+=cut
+
+sub coord_system_name {
+  my ($self, $object) = @_;
+  
+  return $object->{coord_system}{name};
+}
+
+=head2 coord_system_version
+
+=cut
+
+sub coord_system_version {
+  my ($self, $object) = @_;
+  
+  return $object->{coord_system}{version};
+}
+
+=head2 start
+
+    Description: 
     Returntype : String
 
 =cut
 
-sub id {
+sub start {
   my ($self, $object) = @_;
-  exists $object->{id} and defined $object->{id}
-    or croak "Feature does not have id";
   
-  return $object->{id};
+  return $object->{start};
+}
+
+=head2 end
+
+    Description: 
+    Returntype : String
+
+=cut
+
+sub end {
+  my ($self, $object) = @_;
+  
+  return $object->{end};
+}
+
+=head2 strand
+
+    Description: 
+    Returntype : String
+
+=cut
+
+sub strand {
+  my ($self, $object) = @_;
+  
+  return $object->{strand};
+}
+
+=head2 biotype
+
+    Description: 
+    Returntype : String
+
+=cut
+
+sub biotype {
+  my ($self, $object) = @_;
+  
+  return $object->{biotype};
+}
+
+=head2 rank
+
+    Description: 
+    Returntype : String
+
+=cut
+
+sub rank {
+  my ($self, $object) = @_;
+  
+  return $object->{rank};
+}
+
+=head2 taxon_id
+
+    Description: 
+    Returntype : 
+
+=cut
+
+sub taxon_id {
+  my ($self, $object) = @_;
+  
+  return $self->meta_adaptor->get_taxonomy_id;
+}
+
+=head2 uri
+
+    Description: 
+    Returntype : String
+
+=cut
+
+sub uri {
+  my ($self, $object) = @_;
+
+  return feature_uri($self->id($object), $self->type($object));
 }
 
 =head2 synonyms
@@ -222,7 +343,7 @@ sub id {
 sub synonyms {
   my ($self, $object) = @_;
   
-  return $object->{synonyms};
+  return $object->{synonyms} || [];
 }
 
 =head2 provenance
@@ -234,21 +355,13 @@ sub synonyms {
 
 sub provenance {
   my ($self, $object) = @_;
-  
-  return $object->{provenance};
-}
 
-=head2 homologues
+  my $type = $self->type($object);
+  return 'ANNOTATED' if $type eq 'gene';
+  return 'INFERRED_FROM_TRANSCRIPT' if $type eq 'transcript';
+  return 'INFERRED_FROM_TRANSLATION' if $type eq 'translation';
 
-    Description: Wrapper around RDF feature homologues field
-    Returntype : ArrayRef
-
-=cut
-
-sub homologues {
-  my ($self, $object) = @_;
-  
-  return $object->{homologues};
+  croak "Cannot infer provenance for feature of type $type";
 }
 
 =head2 xrefs
@@ -263,6 +376,71 @@ sub xrefs {
   
   return $object->{xrefs};
 }
+
+=head2 dbname
+
+    Description: 
+    Returntype : String
+
+=cut
+
+sub dbname {
+  my ($self, $object) = @_;
+  
+  return $object->{dbname};
+}
+
+=head2 homologues
+
+=cut
+
+sub homologues {
+  my ($self, $object) = @_;
+
+  return $object->{homologues} || [];
+}
+
+=head2 transcripts
+
+=cut
+
+sub transcripts {
+  my ($self, $object) = @_;
+
+  return $object->{transcripts} || [];
+}
+
+=head2 exons
+
+=cut
+
+sub exons {
+  my ($self, $object) = @_;
+
+  return $object->{exons} || [];
+}
+
+=head2 translations
+
+=cut
+
+sub translations {
+  my ($self, $object) = @_;
+
+  return $object->{translations} || [];
+}
+
+=head2 protein_features
+
+=cut
+
+sub protein_features {
+  my ($self, $object) = @_;
+
+  return $object->{protein_features} || [];
+}
+
+
 
 =head2 so_term
 
