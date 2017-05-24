1dc952630dc2a265b833c428f874f9bcd2e594fa
Alessandro Vullo <avullo@ebi.ac.uk>
2017-05-24 15:02:03 +0100

    [ENSCORESW-1928]. Completed code to support writing BulkFetcherFeature types.

--- modules/Bio/EnsEMBL/IO/Writer/RDF.pm
+++ modules/Bio/EnsEMBL/IO/Writer/RDF.pm
@@ -92,17 +92,20 @@ sub create_record {
   my $translator = shift || $self->translator;
   return unless $translator;
 
-  return $self->_create_seq_region_record($object, $translator)
+  return $self->_seq_region_record($object, $translator)
     if $object->isa('Bio::EnsEMBL::Slice');
-    
-  return $self->_create_bulk_fetcher_feature_record($object, $translator);
+
+  my $record;
+  $self->_bulk_fetcher_feature_record($object, $translator, \$record);
+  return $record;
 }
 
-sub _create_seq_region_record {
+sub _seq_region_record {
   my ($class, $object, $translator) = @_;
 
-  my ($version, $region_name, $cs_name, $cs_version, $scientific_name) =
-    $translator->batch_fields($object, [qw/version name cs_name cs_version scientific_name/]);
+  my $version = $translator->version();
+  my ($region_name, $cs_name, $cs_version, $scientific_name) =
+    $translator->batch_fields($object, [qw/name cs_name cs_version scientific_name/]);
   my ($version_uri, $non_version_uri) = $translator->uri($object);
 
   my $record;
@@ -128,12 +131,153 @@ sub _create_seq_region_record {
   return $record;
 }
 
-sub _create_bulk_fetcher_feature_record {
-  my ($class, $object, $translator) = @_;
+# This method calls recursively down the gene->transcript->translation chain and prints them all
+# It can also be used safely with other kinds of features, at least superficially.
+# Any specific vocabulary must be added to describe anything other than the entity and its location
 
-  my $record;
+sub _bulk_fetcher_feature_record {
+  my ($self, $object, $translator, $record) = @_;
+  my $feature_uri = $translator->uri($object);
+  my $feature_type = $translator->type($object);
+  
+  # Translations don't have biotypes. Other features won't either.
+  my $biotype = $translator->biotype($object);
+  if ($biotype) {
+    my $so_term = $translator->so_term($object);
+    ${$record} .= sprintf "%s\n", triple(u($feature_uri), 'rdf:type', 'obo:'.clean_for_uri($so_term)) if $so_term;
+    ${$record} .= sprintf "%s\n", triple(u($feature_uri), 'rdf:type', 'term:'.clean_for_uri($biotype));
+  }
+  
+  my ($feature_id, $feature_name, $feature_description) = $translator->batch_fields($object, [qw/id name description/]);
+  ${$record} .= sprintf "%s\n", triple(u($feature_uri), 'rdfs:label', '"'.$feature_name.'"') if $feature_name;
+  ${$record} .= sprintf "%s\n", triple(u($feature_uri), 'dc:description', '"'.escape($feature_description).'"') if $feature_description;
+  ${$record} .= sprintf "%s\n%s\n",
+    taxon_triple(u($feature_uri), $translator->taxon_id),
+    triple(u($feature_uri), 'dc:identifier', '"'.$feature_id.'"' );
 
-  return $record;
+  # Identifiers.org mappings
+  my $id_org_abbrev = $translator->id_org_short('ensembl');
+  if ($id_org_abbrev) {
+    my $id_org_uri = prefix('identifiers').$id_org_abbrev.'/'.uri_escape($feature_id);
+    ${$record} .= sprintf "%s\n%s\n%s\n",
+      triple(u($feature_uri), 'rdfs:seeAlso', u( $id_org_uri )),
+      triple(u($id_org_uri), 'rdf:type', 'identifiers:'.$id_org_abbrev),
+      triple(u($id_org_uri),'sio:SIO_000671',"[a ident_type:$id_org_abbrev; sio:SIO_000300 \"$feature_id\"]");
+  }
+  
+  # describe location in Faldo
+  $self->_add_faldo_location($object, $translator, $record) unless $feature_type eq 'translation';
+
+  # print out synonyms
+  for my $synonym (@{$translator->synonyms($object)}) {
+    ${$record} .= sprintf "%s\n", triple(u($feature_uri),'skos:altlabel', '"'.escape($synonym).'"' );
+  }
+  
+  if ($feature_type eq 'gene') {
+    # connect gene to transcripts
+    foreach my $transcript (@{$translator->transcripts($object)}) {
+      my $transcript_uri = $translator->uri($transcript);
+      $self->_bulk_fetcher_feature_record($transcript, $translator, $record);
+      ${$record} .= sprintf "%s\n", triple(u($transcript_uri),'obo:SO_transcribed_from', u($feature_uri));
+    }
+    
+    # Homologues come in three types
+    # Orthologues - shared ancestor, same gene different species
+    # Paralogues - same species, unexpected copy not repeatmasked by the assembly
+    # Homeologues - same species, different sub-genome in a polyploid species.
+    foreach my $alt_gene (@{$translator->homologues($object)}) {
+      my $predicate = ($alt_gene->{description} eq 'within_species_paralog') ? 'sio:SIO:000630': 'sio:SIO_000558';
+      ${$record} .= sprintf "%s\n", triple(u($feature_uri), $predicate, 'ensembl:'.$alt_gene->{stable_id});
+    }
+  }
+
+  if ($feature_type eq 'transcript') {
+    # connect transcript to exons
+    foreach my $exon (@{$translator->exons($object)}) {
+      # exon type of SO exon, both gene and transcript are linked via has part
+      my $exon_uri = $translator->uri($exon);
+      
+      ${$record} .= sprintf "%s\n%s\n%s\n",
+	triple(u($exon_uri),'rdf:type','obo:SO_0000147'),
+	triple(u($exon_uri), 'rdfs:label', '"'.$translator->id($exon).'"'),
+	triple(u($feature_uri), 'obo:SO_has_part', u($exon_uri));
+      
+      $self->_bulk_fetcher_feature_record($exon, $translator, $record);
+      
+      my $rank = $translator->rank($object);
+      ${$record} .= sprintf "%s\n%s\n%s\n%s\n",
+	triple(u($feature_uri), 'sio:SIO_000974',  u($feature_uri.'#Exon_'.$rank)),
+	triple(u($feature_uri.'#Exon_'.$rank),  'rdf:type', 'sio:SIO_001261'),
+	triple(u($feature_uri.'#Exon_'.$rank), 'sio:SIO_000628', u($exon_uri)),
+	triple(u($feature_uri.'#Exon_'.$rank), 'sio:SIO_000300', $rank);
+    }
+
+    # connect transcript to translations
+    foreach my $translation (@{$translator->translations($object)}) {
+      $self->_bulk_fetcher_feature_record($translation, $translator, $record);
+	
+      my $translation_uri = $translator->uri($translation);
+      ${$record} .= sprintf "%s\n%s\n",
+	triple(u($feature_uri),'obo:SO_translates_to',u($translation_uri)),
+	triple(u($translation_uri), 'rdf:type', 'term:protein');
+	
+      foreach my $protein_feature (@{$translator->protein_features($translation)}) {
+	my $pf_name = $translator->name($protein_feature);
+	my $pf_dbname = $translator->dbname($protein_feature);
+	next unless defined $pf_name && defined $pf_dbname;
+	$pf_dbname = lc($pf_dbname);
+
+	${$record} .= sprintf "%s\n", triple(u($translation_uri), 'rdfs:seeAlso', $pf_dbname.':'.$pf_name)
+	  if defined prefix($pf_dbname);
+      }
+    }
+    
+  }
+  
+}
+
+sub _add_faldo_location {
+  my ($self, $object, $translator, $record) = @_;
+  
+  my ($feature_uri, $region_name, $cs_name, $cs_version, $start, $end, $strand) =
+    $translator->batch_fields($object, [qw/uri seq_region_name cs_name cs_version start end strand/]);
+  my ($schema_version, $production_name) = ($translator->release, $translator->production_name);
+
+  my $prefix = prefix('ensembl');
+  unless (defined $schema_version && defined $region_name && defined $cs_name) {
+    croak ('Cannot print location triple without seq_region_name, coord_system name, and a release');
+  }
+  # LRGs have their own special seq regions... they may not make a lot of sense
+  # in the RDF context.
+  # The same is true of toplevel contigs in other species.
+  my ($version_uri, $unversioned_uri) = seq_region_uri($translator->version, $translator->production_name, $cs_version, $region_name);
+  
+  my $begin = ($strand >= 0) ? $start : $end;
+  my $stop = ($strand >= 0) ? $end : $start;
+  my $location = seq_region_uri($schema_version, $production_name, $cs_version, $region_name, $start, $end, $strand);
+  my $beginUri = seq_region_uri($schema_version, $production_name, $cs_version, $region_name, $begin, undef, $strand);
+  my $endUri = seq_region_uri($schema_version, $production_name, $cs_version, $region_name, undef, $stop, $strand);
+  
+  ${$record} .= sprintf "%s\n%s\n%s\n%s\n%s\n%s\n",
+    triple(u($feature_uri), 'faldo:location', $location),
+    triple($location, 'rdfs:label', qq("$cs_name $region_name:$start-$end:$strand")),
+    triple($location, 'rdf:type', 'faldo:Region'),
+    triple($location, 'faldo:begin', $beginUri),
+    triple($location, 'faldo:end', $endUri),
+    triple($location, 'faldo:reference', $version_uri);
+  
+  ${$record} .= sprintf "%s\n%s\n%s\n%s\n",
+    triple($beginUri, 'rdf:type', 'faldo:ExactPosition'),
+    triple($beginUri, 'rdf:type', ($strand >= 0)? 'faldo:ForwardStrandPosition':'faldo:ReverseStrandPosition'),
+    triple($beginUri, 'faldo:position', $begin),
+    triple($beginUri, 'faldo:reference', $version_uri);
+
+  ${$record} .= sprintf "%s\n%s\n%s\n%s\n",
+    triple($endUri, 'rdf:type', 'faldo:ExactPosition'),
+    triple($endUri, 'rdf:type', ($strand >= 0)? 'faldo:ForwardStrandPosition':'faldo:ReverseStrandPosition'),
+    triple($endUri, 'faldo:position', $stop),
+    triple($endUri, 'faldo:reference', $version_uri);
+  
 }
 
 1;
