ec7ca34de5c13031f6dcb0aa2f918f59522c76f4
Irina Armean <iarmean@ebi.ac.uk>
2018-09-19 13:45:30 +0100

    added test and simplified if

--- modules/Bio/EnsEMBL/IO/TabixParser.pm
+++ modules/Bio/EnsEMBL/IO/TabixParser.pm
@@ -49,7 +49,7 @@ sub open {
 
   my $self = $class->SUPER::new(@other_args);
 
-  confess "ERROR: Input file is not bgzipped, cannot use tabix\n" unless $filename =~ /\.gz$/ || $filename =~ /\.bgz$/;
+  confess "ERROR: Input file is not bgzipped, cannot use tabix\n" unless $filename =~ /\.b?gz$/;
 
   my %params = (filename => $filename);
   ## Tabix will want to write the downloaded index file to 
